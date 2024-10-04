6ecaa1728b6067633863c9dab83eb45c07b02976
Moulitharann <117554286+Moulitharann@users.noreply.github.com>
2024-10-04 19:51:19 +0530

    Time: 15 ms (76.57%), Space: 58.4 MB (8.06%) - LeetHub

--- 2581-divide-players-into-teams-of-equal-skill/2581-divide-players-into-teams-of-equal-skill.java
+++ 2581-divide-players-into-teams-of-equal-skill/2581-divide-players-into-teams-of-equal-skill.java
@@ -1,22 +1,21 @@
-import java.util.Arrays;
-
 class Solution {
     public long dividePlayers(int[] skill) {
-        
         Arrays.sort(skill);
-        
-        int n = skill.length;
-        int totalSkill = skill[0] + skill[n - 1]; 
-        long chemistrySum = 0;
-
-       for (int i = 0; i < n / 2; i++) {
-             if (skill[i] + skill[n - i - 1] != totalSkill) {
-                return -1; 
+        int left = 0;
+        int right = skill.length - 1;
+        int sum = skill[left] + skill[right];
+        long res = 0;
+        while (left < right){
+            if (skill[left] + skill[right] != sum){
+                return -1;
             }
-           
-            chemistrySum += (long) skill[i] * skill[n - i - 1];
-        }
+            else{
+                res += (skill[left] * skill[right]);
+            }
+            left ++;
+            right --;
 
-        return chemistrySum; 
+        }
+        return res;
     }
 }
