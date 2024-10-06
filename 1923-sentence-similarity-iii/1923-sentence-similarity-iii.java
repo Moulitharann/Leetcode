7827b08275e55956f7ef3193694b708cfbbb955a
Moulitharann <117554286+Moulitharann@users.noreply.github.com>
2024-10-06 12:11:09 +0530

    Time: 1 ms (96.43%), Space: 41.9 MB (13.89%) - LeetHub

--- 1923-sentence-similarity-iii/1923-sentence-similarity-iii.java
+++ 1923-sentence-similarity-iii/1923-sentence-similarity-iii.java
@@ -1,7 +1,6 @@
 class Solution {
     public boolean areSentencesSimilar(String s1, String s2) {
-        // Split both sentences into words
-        String[] words1 = s1.split(" ");
+         String[] words1 = s1.split(" ");
         String[] words2 = s2.split(" ");
         if (words1.length < words2.length) {
             String[] temp = words1;
@@ -11,19 +10,16 @@ class Solution {
         int i = 0, j = words1.length - 1;
         int k = 0, l = words2.length - 1;
 
-        // Check prefix match from the beginning of both sentences
-        while (i <= j && k <= l && words1[i].equals(words2[k])) {
+         while (i <= j && k <= l && words1[i].equals(words2[k])) {
             i++;
             k++;
         }
         
-        // Check suffix match from the end of both sentences
         while (i <= j && k <= l && words1[j].equals(words2[l])) {
             j--;
             l--;
         }
         
-        // After checking both prefix and suffix, if all words have been matched, the sentences are similar
-        return k > l;
+       return k > l;
     }
 }
