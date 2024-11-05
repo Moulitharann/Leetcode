0b4351e2a0c90642f7184da826d2b617a8386d79
Moulitharann <117554286+Moulitharann@users.noreply.github.com>
2024-11-05 20:20:59 +0530

    Time: 3 ms (97.78%), Space: 45 MB (17.19%) - LeetHub

--- 3174-minimum-number-of-changes-to-make-binary-string-beautiful/3174-minimum-number-of-changes-to-make-binary-string-beautiful.java
+++ 3174-minimum-number-of-changes-to-make-binary-string-beautiful/3174-minimum-number-of-changes-to-make-binary-string-beautiful.java
@@ -1,34 +1,11 @@
 class Solution {
     public int minChanges(String s) {
-     List<Integer> odd = new ArrayList<>();
-        int n = s.length();
-        
-        int i = 1;
-        int start = 0;
-        int count = 0;
-        while (i < n) {
-            while (i < n && s.charAt(i) == s.charAt(i - 1)) {
-                i++;
+        int res = 0;
+        for (int i = 1; i < s.length(); i += 2) {
+            if (s.charAt(i - 1) != s.charAt(i)) {
+                res++;
             }
-            
-            if ((i - start) % 2 == 1) {
-                odd.add(count);
-            }
-            
-            count++;
-            start = i;
-            i++;
         }
-        
-        if ((i - start) % 2 == 1) {
-            odd.add(count);
-        }
-
-        int minChanges = 0;
-        for (int pos = 1; pos < odd.size(); pos += 2) {
-            minChanges += odd.get(pos) - odd.get(pos - 1);
-        }
-        
-        return minChanges;
+        return res;
     }
 }
