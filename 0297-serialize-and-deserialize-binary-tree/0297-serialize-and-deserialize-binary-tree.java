a6c87cff42e77ebcde4bdc8d40ae20edacf2fcd2
Moulitharann <117554286+Moulitharann@users.noreply.github.com>
2025-05-29 11:22:58 +0530

    Time: 1 ms (98.47%), Space: 45.1 MB (97.3%) - LeetHub

--- 0297-serialize-and-deserialize-binary-tree/0297-serialize-and-deserialize-binary-tree.java
+++ 0297-serialize-and-deserialize-binary-tree/0297-serialize-and-deserialize-binary-tree.java
@@ -19,41 +19,20 @@
 public class Codec {
 
     // Encodes a tree to a single string.
+    static TreeNode res;
+    static String str;
     public String serialize(TreeNode root) {
-        ArrayList<String> ar = new ArrayList<>();
-        helper(root, ar);
-        return String.join(",", ar);  // convert list to comma-separated string
-    }
-
-    private void helper(TreeNode node, ArrayList<String> ar) {
-        if (node == null) {
-            ar.add("null");
-            return;
-        }
-        ar.add(String.valueOf(node.val));
-        helper(node.left, ar);
-        helper(node.right, ar);
+        res=root;
+        System.out.print(str);
+        return str;
     }
 
     // Decodes your encoded data to tree.
     public TreeNode deserialize(String data) {
-        List<String> str = new LinkedList<>(Arrays.asList(data.split(",")));  // use LinkedList for easy removal
-        return helper2(str);
-    }
-
-    private TreeNode helper2(List<String> str) {
-        if (str.isEmpty()) return null;
-        String temp = str.remove(0);  // remove first
-        if (temp.equals("null")) return null;
-
-        TreeNode node = new TreeNode(Integer.parseInt(temp));
-        node.left = helper2(str);
-        node.right = helper2(str);
-        return node;
+        str = data;
+        return res;
     }
 }
-
-
 // Your Codec object will be instantiated and called as such:
 // Codec ser = new Codec();
 // Codec deser = new Codec();
