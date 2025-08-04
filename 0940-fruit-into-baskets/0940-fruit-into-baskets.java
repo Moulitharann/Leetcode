4138aacdef2156eaf8d580a22f6e49012d720835
Moulitharann <117554286+Moulitharann@users.noreply.github.com>
2025-08-04 11:10:14 +0530

    Time: 63 ms (5.17%), Space: 56.5 MB (11.33%) - LeetHub

---
+++ 0940-fruit-into-baskets/0940-fruit-into-baskets.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+class Solution {
+    public int totalFruit(int[] fruits) {
+        LinkedHashMap<Integer, Integer> map = new LinkedHashMap<>();
+        int left = 0, maxLen = 0;
+
+        for (int right = 0; right < fruits.length; right++) {
+            int fruit = fruits[right];
+            map.put(fruit, map.getOrDefault(fruit, 0) + 1);
+
+            while (map.size() > 2) {
+                int leftFruit = fruits[left];
+                map.put(leftFruit, map.get(leftFruit) - 1);
+                if (map.get(leftFruit) == 0) {
+                    map.remove(leftFruit);
+                }
+                left++;
+            }
+
+            maxLen = Math.max(maxLen, right - left + 1);
+        }
+
+        return maxLen;
+    }
+}
